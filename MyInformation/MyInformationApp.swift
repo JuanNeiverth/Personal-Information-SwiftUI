1787cd124dea45e59879eac42354c7a43722fbf7
JuanNeiverth <polli.neiverth@gmail.com>
2023-03-20 15:35:35 -0500

    Initial Commit

---
+++ MyInformation/MyInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyInformationApp.swift
+//  MyInformation
+//
+//  Created by Juan on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MyInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
